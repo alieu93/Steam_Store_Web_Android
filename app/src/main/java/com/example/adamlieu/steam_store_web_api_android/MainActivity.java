c65dd840c6d3d0ce8d9621ac03307aea1297e2c2
alieu93 <adam.lieu@hotmail.com>
2017-02-27 23:49:09 -0500

    Added in ListView to hold the retrieved contents

--- app/src/main/java/com/example/adamlieu/steam_store_web_api_android/MainActivity.java
+++ app/src/main/java/com/example/adamlieu/steam_store_web_api_android/MainActivity.java
@@ -3,36 +3,40 @@ package com.example.adamlieu.steam_store_web_api_android;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> listGames = new ArrayList<String>();
+    private ListView listView;
+
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview1);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        listView.setAdapter(adapter);
+
         new Initialize().execute();
     }
 
     private class Initialize extends AsyncTask<Void, Void, String> {
-        String ht = "";
+        //String ht = "";
         protected String doInBackground(Void... params){
             try {
                 /*URL url = new URL("http://store.steampowered.com/search/?filter=comingsoon");
@@ -47,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
                 Elements releaseDate = content.getElementsByAttributeValueContaining("class", "col search_released responsive_secondrow");
 
                 for(int i = 0; i < titleName.size(); i++){
-                    ht += titleName.get(i).getElementsByClass("title").text() + "\n" + "- " + releaseDate.get(i).text() + "\n";
+                    //ht += titleName.get(i).getElementsByClass("title").text() + "\n" + "- " + releaseDate.get(i).text() + "\n";
+                    listGames.add(titleName.get(i).getElementsByClass("title").text() + "\n" + "- " + releaseDate.get(i).text());
                 }
             } catch (MalformedURLException e) {
                 Log.e("MalformedURL: ", e.toString());
@@ -56,50 +61,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-            return ht;
+            return null;
         }
 
         protected void onPostExecute(String result){
-            TextView t = (TextView) findViewById(R.id.textview1);
-            //t.setText(ht);
+           /* TextView t = (TextView) findViewById(R.id.textview1);
             t.setText(ht);
-            t.setMovementMethod(new ScrollingMovementMethod());
+            t.setMovementMethod(new ScrollingMovementMethod());*/
+            adapter.addAll(listGames);
+            adapter.notifyDataSetChanged();
         }
     }
-
-    private String readStream(InputStream in){
-        BufferedReader br = null;
-        StringBuilder sb = new StringBuilder();
-        String line;
-        boolean flag = false;
-
-        try {
-            br = new BufferedReader(new InputStreamReader(in));
-            while((line = br.readLine()) != null){
-                //TODO: find some way to filter out everything but whatever falls into this div category
-                if(line.contains("search_result_container")){
-                    flag = true;
-                }
-                if(flag) sb.append(line);
-
-            }
-        } catch(IOException e){
-            Log.e("BufferedReader", e.toString());
-        } finally {
-            if(br != null){
-                try {
-                    br.close();
-                } catch (IOException e){
-                    Log.e("BufferedReader close", e.toString());
-                }
-            }
-        }
-
-        Log.e("readStream", sb.toString());
-        return sb.toString();
-        //TextView text = (TextView) findViewById(R.id.textview1);
-        //text.setText(sb.toString());
-    }
-
 
 }
